2d00b9b1f8380713fdbe468cc141b4b87e17c43d
LUC457 <123959612+LUC457@users.noreply.github.com>
2023-02-06 13:37:24 +0200

    FIRST COMMIT FOR MY PROJECT

---
+++ inde.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="node models/vue/dist/vue.global.js"></script>
+    <style>
+        body {
+            background-color: gray;
+            font-family: "tahoma";
+            color: white;
+        }
+
+        #leftpanel {
+            background-color:gray;
+        }
+        </style>
+
+        </head><body><div id="title">the title will be placed here </div><div id="leftpaneel"><em>Lorem ipsum dolor,
+        sit amet consectetur adipisicing elit. Quibusdam voluptatibus aliquam pariatur veritatis natus totam ! Rem molestias delectus accusantium quisquam non magni accusamus ! Vero itaque magnam velit officia quam quia.</em></div><div id="main">product and contents will go here</div></body></html>
